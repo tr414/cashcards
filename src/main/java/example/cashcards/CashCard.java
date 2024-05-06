4055b11d40b5f06b60ff9753b98fc749298acf21
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-05-06 16:20:42 +0800

    Wrote first few tests for JSON serialization and deserialization using JacksonTester

---
+++ src/main/java/example/cashcards/CashCard.java
@@ -0,0 +1,12 @@
+package example.cashcards;
+
+record CashCard(Long id, Double amount) {
+}
+
+//public class CashCard {
+//    public CashCard() {
+//    }
+//
+//    public CashCard(Long id, Double amount) {
+//    }
+//}
