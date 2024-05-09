9530b26add61b89a3a1bcb38fd2f3fde79e801df
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-05-09 20:37:55 +0800

    Add data and GET and POST request endpoints

--- src/main/java/example/cashcards/CashCardController.java
+++ src/main/java/example/cashcards/CashCardController.java
@@ -1,17 +1,50 @@
 package example.cashcards;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private final CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        // create a new cashcard and save it to the database
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+
+        // construct a uri that redirects the user to the cashcard that was just created
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+
+        // return a response with the status 201 Created, the location in the header.
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping()
+    private ResponseEntity<Iterable<CashCard>> findAll() {
+        return ResponseEntity.ok(cashCardRepository.findAll());
     }
 }
--- src/test/java/example/cashcards/CashCardsApplicationTests.java
+++ src/test/java/example/cashcards/CashCardsApplicationTests.java
@@ -2,6 +2,8 @@ package example.cashcards;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import org.assertj.core.api.Assertions;
+import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import  org.assertj.core.api.Assert;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardsApplicationTests {
@@ -47,6 +52,50 @@ class CashCardsApplicationTests {
 
 		// ensure that the id of the object that is returned is what we expect it to be (99 in this case)
 		assertThat(id).isEqualTo(99);
+	}
+
+	@Test
+	void shouldNotReturnACashCardWithUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+		// the below test fails when we run it. What is the difference between isBlank and isEmpty?
+		// assertThat(response.getBody()).isEmpty();
+	}
+
+	@Test
+	void shouldCreateCashCard() {
+		CashCard newCashCard = new CashCard(null, 250.0);
+		// create a post request to the cashcards endpoint with the newCashCard provided. The response body will not
+		// contain anything, hence the final argument is Void.class
+		ResponseEntity<Void> response = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewCashCard = response.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		// check to verify that the card that is returned by the Get request to the uri provided in the
+		// POST request response is valid and has the same amount as what was provided in the test
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		Double amount = documentContext.read("$.amount");
+
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(250.0);
+	}
+
+	@Test
+	void shouldReturnAllCashCardsWhenListIsRequested() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		int cashCardCount = documentContext.read("$.length()");
+		assertThat(cashCardCount).isEqualTo(3);
+
+		JSONArray ids = documentContext.read("$..id");
+		//assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
 	}
 
 }
